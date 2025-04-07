56acc93b481a15d5fbfa3e7120f20ba1aac736de
Mamiko.Nakamura <mamiko.nakamura@nex.nikkei.com>
2025-04-07 17:17:14 +0900

    Initial commit

---
+++ testTests/testTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by Mamiko Nakamura on 2024/11/20.
+//
+
+import Testing
+@testable import test
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
